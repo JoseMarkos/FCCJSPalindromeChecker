988803f02c4970661c471af91bae1f359e4341f7
Marcos <jkmarcosb@gmail.com>
2021-05-24 23:49:47 -0600

    fixing func name

--- filter.ts
+++ filter.ts
@@ -1,6 +1,4 @@
-const filterNonAlpahNumerics = (str: string): string => {
+export default function (str: string): string {
   const match = str.match(/[a-z0-9]+/gi);
   return match ? match.join('') : str;
 }
-
-export {filterNonAlpahNumerics}
