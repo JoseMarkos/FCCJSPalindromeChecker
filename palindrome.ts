6790465f3fabeb2b0fa9116307ddc10f3e1d1def
Marcos <jkmarcosb@gmail.com>
2021-05-24 23:50:31 -0600

    adding gendle reading and kind of clean code

--- palindrome.ts
+++ palindrome.ts
@@ -1,18 +1,4 @@
-import { filterNonAlpahNumerics } from "./filter.ts"
-
-const format = (str: string): string => filterNonAlpahNumerics(str).toLowerCase();
-
-const split = (str: string): string[] => {
-  let middle: number = 0;
-  
-  if (str.length % 2 === 0) {
-    middle = str.length / 2;
-    return [str.slice(0, middle), str.slice(middle)];
-  }
-
-  middle = (str.length - 1) / 2;
-  return [str.slice(0, middle), str.slice(middle + 1)];
-}
+import filterNonAlphaNumerics from "./filter.ts"
 
 export default function (str: string): boolean {
   let formated = format(str);
@@ -23,11 +9,29 @@ export default function (str: string): boolean {
   if (formated[0] !== formated[formated.length - 1])
     return false
   
-  let filtered      = split(formated);
-  let reverseIndex  = filtered[0].length - 1;
+  let splited = splitIntoTwo(formated);
 
-  for (let index = 0; index < filtered[0].length; index++) {
-    if (filtered[0][index] !== filtered[1][reverseIndex]) {
+  return isPalindrome(splited);
+}
+
+const format = (str: string): string => filterNonAlphaNumerics(str).toLowerCase();
+
+const splitIntoTwo = (str: string): string[] => {
+  let middle: number = (str.length - 1) / 2;
+  
+  if (str.length % 2 === 0) {
+    middle = str.length / 2;
+    return [str.slice(0, middle), str.slice(middle)];
+  }
+
+  return [str.slice(0, middle), str.slice(middle + 1)];
+}
+
+const isPalindrome = (arr: string[]): boolean => {
+  let reverseIndex  = arr[0].length - 1;
+
+  for (let index = 0; index < arr[0].length; index++) {
+    if (arr[0][index] !== arr[1][reverseIndex]) {
       return false;
     }
     
